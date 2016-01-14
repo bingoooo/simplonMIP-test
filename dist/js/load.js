ae98efa267525547c240e34685e24c95c6a68335
Dant Benjamin <sukkhato@gmail.com>
2016-01-14 23:43:57 +0100

    Création de la distribution première du site

---
+++ dist/js/load.js
@@ -0,0 +1,10 @@
+'use strict';
+$(function() {
+	$("#main_menu a").click(function(e){
+		e.preventDefault();
+		var fichier = $(this).attr('href');
+		$("section").load("partials/" + fichier + ".html");
+		$("#main_menu .active").removeClass("active");
+		$(this).addClass("active");
+	});
+});
